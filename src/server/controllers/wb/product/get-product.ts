2ccf2519cd87cec5c1d928490bf859403594118b
tehzombie <tehzombie@tehzombie.tehzombie>
2023-10-08 16:46:51 +0300

    fix product and image fetch

--- src/server/controllers/wb/product/get-product.ts
+++ src/server/controllers/wb/product/get-product.ts
@@ -4,11 +4,10 @@ import { smartFetch } from '../../../helpers/smart-fetch';
 import { WBProductResult } from './models/product-result.interface';
 
 export async function getWBProduct(id: string): Promise<WBProductResult> {
-  const volume = id.substring(0, 4);
-  const part = id.substring(0, 6);
+  const volume = ~~(+id / 1e5);
+  const part = ~~(+id / 1e3);
   const shard = getHostV2(+volume);
   const url = `https://${shard}.wb.ru/vol${volume}/part${part}/${id}/info/ru/card.json`;
-  console.log(url);
   const response = await smartFetch(null, url);
   if (!response?.ok) {
     const [parseError, errorBody] = await caught(response?.json());
