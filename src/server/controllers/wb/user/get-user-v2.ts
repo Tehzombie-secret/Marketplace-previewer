7a4eeb36cdcf410fb6bcd61fd4d12d227cbc683c
tehzombie <tehzombie@tehzombie.tehzombie>
2023-10-10 13:09:08 +0300

    deduplicate feedbacks

--- src/server/controllers/wb/user/get-user-v2.ts
+++ src/server/controllers/wb/user/get-user-v2.ts
@@ -4,16 +4,14 @@ import { UserFeedback } from '../../../../app/models/feedbacks/user-feedback.int
 import { Person } from '../../../../app/models/person/person.interface';
 import { APIPlatform } from '../../../../app/services/api/models/api-platform.enum';
 import { ImageSize } from '../../../models/image-size.enum';
+import { FeedbacksSchema } from '../../../services/mongodb/models/collection-schemas/feedbacks-schema.interface';
 import { MongoDBCollection } from '../../../services/mongodb/models/mongo-db-collection.enum';
 import { MongoDBService } from '../../../services/mongodb/mongodb.service';
 import { UserResponse } from './models/user-response.interface';
 
 export async function getWBUserV2(mongoDB: MongoDBService, id: string, useGlobalId?: boolean): Promise<UserResponse> {
   const dbFeedbacks = typeof useGlobalId !== 'boolean'
-    ? [
-      ...await mongoDB.read(MongoDBCollection.FEEDBACKS, 'uId', id),
-      ...await mongoDB.read(MongoDBCollection.FEEDBACKS, 'uWId', id),
-    ]
+    ? await fetchAllFeedbacks(mongoDB, id)
     : await mongoDB.read(MongoDBCollection.FEEDBACKS, useGlobalId ? 'uId' : 'uWId', id);
   const feedbacks = dbFeedbacks
     .map((item) => {
@@ -52,4 +50,14 @@ export async function getWBUserV2(mongoDB: MongoDBService, id: string, useGlobal
     response: result,
     status: 200,
   };
+}
+
+async function fetchAllFeedbacks(mongoDB: MongoDBService, id: string): Promise<FeedbacksSchema[]> {
+  const dbFeedbacks = [
+    ...await mongoDB.read(MongoDBCollection.FEEDBACKS, 'uWId', id),
+    ...await mongoDB.read(MongoDBCollection.FEEDBACKS, 'uWId', id),
+  ];
+  const idToFeedbackMap = new Map<string, FeedbacksSchema>(dbFeedbacks.map((item) => [item.id, item]));
+  const result = Array.from(idToFeedbackMap.values());
+  return result;
 }
