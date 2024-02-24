8fc593b7f51323226d312476474b6f5e8d7833c1
tehzombie <tehzombie@tehzombie.tehzombie>
2024-02-24 13:38:33 +0300

    start traverse with phase

--- src/server/helpers/profile-traverser/listen-on-traverse-demands.ts
+++ src/server/helpers/profile-traverser/listen-on-traverse-demands.ts
@@ -1,13 +1,20 @@
 import { interval, startWith } from 'rxjs';
+import { TraverseStatus } from '../../services/mongodb/models/traverse-status.enum';
 import { MongoDBService } from '../../services/mongodb/mongodb.service';
 import { startTraverse } from './start-traverse';
 
-export function listenOnTraverseDemands(mongoDB: MongoDBService): void {
+export function listenOnTraverseDemands(mongoDB: MongoDBService, startWithStatus?: TraverseStatus): void {
+  let started = false;
   interval(60 * 1000) // Every minute
     .pipe(
       startWith(1),
     )
     .subscribe(async () => {
-      await startTraverse(mongoDB);
+      if (!started && startWithStatus) {
+        started = true;
+        await startTraverse(mongoDB, startWithStatus);
+      } else {
+        await startTraverse(mongoDB);
+      }
     });
 }
